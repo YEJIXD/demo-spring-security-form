166274f997f6de8ac090622912cb75849cf833ed
윤예지 <yedoong919@gmail.com>
2023-11-14 23:36:22 +0900

    [수정] Security 설정 파일 추가 및 버전에 맞게 커스텀 적용

---
+++ src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package me.whiteship.demospringsecurityform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // HttpSecurity 설정
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    // WebSecurity 설정
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
+    }
+
+}
