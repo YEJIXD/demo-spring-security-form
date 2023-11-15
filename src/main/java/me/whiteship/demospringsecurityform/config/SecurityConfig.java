22c607cf5b3321d723ca5b7142686523bd626f81
윤예지 <yedoong919@gmail.com>
2023-11-15 15:53:52 +0900

    [ADD] SecurityConfig 설정 파일 추가

--- src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -2,32 +2,43 @@ package me.whiteship.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // HttpSecurity 설정
+    /**
+     * Security Filter Chain
+     *
+     * @param http
+     *              HttpSecurity
+     * @return Security Filter Chain
+     * @throws Exception
+     */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /**
+         * 접근 권한 설정
+         */
+        http.authorizeHttpRequests(authorize  -> {
+            authorize
+                    // 인증 없이 접근 허용
+                    .requestMatchers("/css/**", "/js/**", "/img/**", "/font/**", "/","/info").permitAll()
+                    // ADMIN 권한이 있어야 허용
+                    .requestMatchers("/admin").hasRole("ADMIN")
+                    // '인증'만 되면 허용
+                    .anyRequest().authenticated();
+        });
+
+        /**
+         * 로그인
+         */
+        http.formLogin(Customizer.withDefaults());
+
         return http.build();
     }
-
-    // WebSecurity 설정
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
-    }
-
 }
